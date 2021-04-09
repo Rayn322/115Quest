19fb46aa0c2d836022ecd5f2e38090d7a983cd02
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2021-04-09 10:21:45 -0700

    simplified setting the score text

--- buildBMBF.ps1
+++ buildBMBF.ps1
@@ -2,5 +2,5 @@
 & $PSScriptRoot/build.ps1
 
 if ($?) {
-    Compress-Archive -Path "./libs/arm64-v8a/lib115Mod.so", "./libs/arm64-v8a/libbeatsaber-hook_1_2_4.so", "./bmbfmod.json" -DestinationPath "./115Mod_v1.0.0.zip" -Update
+    Compress-Archive -Path "./libs/arm64-v8a/lib115Mod.so", "./libs/arm64-v8a/libbeatsaber-hook_1_2_6.so", "./bmbfmod.json" -DestinationPath "./115Mod_v1.0.0.zip" -Update
 }
