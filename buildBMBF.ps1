cfba2c2cd1098b5586835059f1f4aa5d104d0ced
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2021-04-11 13:56:13 -0700

    Updated to latest modding template

--- buildBMBF.ps1
+++
@@ -1,6 +0,0 @@
-# Builds a .zip file for loading with BMBF
-& $PSScriptRoot/build.ps1
-
-if ($?) {
-    Compress-Archive -Path "./libs/arm64-v8a/lib115Mod.so", "./libs/arm64-v8a/libbeatsaber-hook_1_2_6.so", "./bmbfmod.json" -DestinationPath "./115Mod_v1.0.0.zip" -Update
-}
